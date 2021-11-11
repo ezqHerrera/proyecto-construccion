0f4ad3b1c4bffecd0c7c962ec9c1201d00126302
Herrera <rodherrera14@gmail.com>
2021-11-11 11:33:14 -0300

    n-version-0

--- construccion-v2/README.md
+++ construccion-v2/README.md
@@ -1,4 +1,4 @@
-# Construccion
+# ConstruccionV2
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.6.
 
--- construccion-v2/src/app/app.component.spec.ts
+++ construccion-v2/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'construccion'`, () => {
+  it(`should have as title 'construccion-v2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('construccion');
+    expect(app.title).toEqual('construccion-v2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('construccion app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('construccion-v2 app is running!');
   });
 });
