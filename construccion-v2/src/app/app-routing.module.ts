db5fc62fb58eb6fbc9ecd82a6307488c087d1836
Herrera <rodherrera14@gmail.com>
2022-02-15 09:32:34 -0300

    Frontend final n-version-2

--- construccion-v2/src/app/app-routing.module.ts
+++ construccion-v2/src/app/app-routing.module.ts
@@ -8,7 +8,13 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      onSameUrlNavigation:"ignore",
+      anchorScrolling:'enabled',
+      scrollPositionRestoration:'enabled'
+  })
+],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
